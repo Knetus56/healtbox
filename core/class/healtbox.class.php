b3e54d1540accb8216b2e4282720ba9a7e2fecf4
Knetus56 <98704728+Knetus56@users.noreply.github.com>
2023-01-26 12:47:11 +0100

    Update healtbox.class.php

--- core/class/healtbox.class.php
+++ core/class/healtbox.class.php
@@ -1,120 +1,72 @@
 <?php
 /* This file is part of Jeedom.
-*
-* Jeedom is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Jeedom is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
-*/
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* * ***************************Includes********************************* */
-require_once __DIR__  . '/../../../../core/php/core.inc.php';
+require_once __DIR__ . '/../../../../core/php/core.inc.php';
 require_once __DIR__ . '/../../3rdparty/healtbox_api.class.php';
 
-class healtbox extends eqLogic {
-  /*     * *************************Attributs****************************** */
+class healtbox extends eqLogic
+{
 
-  /*
-  * Permet de définir les possibilités de personnalisation du widget (en cas d'utilisation de la fonction 'toHtml' par exemple)
-  * Tableau multidimensionnel - exemple: array('custom' => true, 'custom::layout' => false)
-  public static $_widgetPossibility = array();
-  */
-
-  /*
-  * Permet de crypter/décrypter automatiquement des champs de configuration du plugin
-  * Exemple : "param1" & "param2" seront cryptés mais pas "param3"
-  public static $_encryptConfigKey = array('param1', 'param2');
-  */
-
-  /*     * ***********************Methode static*************************** */
-
-  /*
-  * Fonction exécutée automatiquement toutes les minutes par Jeedom
-  public static function cron() {}
-  */
-
-  /*
-  * Fonction exécutée automatiquement toutes les 5 minutes par Jeedom
-  public static function cron5() {}
-  */
-
-  /*
-  * Fonction exécutée automatiquement toutes les 10 minutes par Jeedom
-  public static function cron10() {}
-  */
-
-  /*
-  * Fonction exécutée automatiquement toutes les 15 minutes par Jeedom
-  public static function cron15() {}
-  */
-
-  /*
-  * Fonction exécutée automatiquement toutes les 30 minutes par Jeedom
-  public static function cron30() {}
-  */
-
-  /*
-  * Fonction exécutée automatiquement toutes les heures par Jeedom
-  public static function cronHourly() {}
-  */
-
-  /*
-  * Fonction exécutée automatiquement tous les jours par Jeedom
-  public static function cronDaily() {}
-  */
-
-  /*     * *********************Méthodes d'instance************************* */
 
   // Fonction exécutée automatiquement avant la création de l'équipement
-  public function preInsert() {
+  public function preInsert()
+  {
   }
 
   // Fonction exécutée automatiquement après la création de l'équipement
-  public function postInsert() {
+  public function postInsert()
+  {
   }
 
   // Fonction exécutée automatiquement avant la mise à jour de l'équipement
-  public function preUpdate() {
+  public function preUpdate()
+  {
     if ($this->getConfiguration('ip') == '') {
       throw new Exception(__('Veuillez entrer une IP', __FILE__));
     }
   }
 
   // Fonction exécutée automatiquement après la mise à jour de l'équipement
-  public function postUpdate() {
+  public function postUpdate()
+  {
   }
 
   // Fonction exécutée automatiquement avant la sauvegarde (création ou mise à jour) de l'équipement
-  public function preSave() {
+  public function preSave()
+  {
   }
 
   // Fonction exécutée automatiquement après la sauvegarde (création ou mise à jour) de l'équipement
-  public function postSave() {
+  public function postSave()
+  {
     $api = new healtbox_api($this->getConfiguration('ip'));
     $ap = $api->getNbPiece();
-    log::add('healtbox','info', $ap );
-   
+    log::add('healtbox', 'info', $ap);
+
     for ($i = 1; $i <= $ap; $i++) {
-     
+
       $NamePiece = $api->getNamePiece();
-     
-      log::add('healtbox','info', $NamePiece );
-     
-     
-  
-     
-     
+
+      log::add('healtbox', 'info', $NamePiece);
+
     }
-   
-     
+
+
     $air = $this->getCmd(null, 'device_type');
     if (!is_object($air)) {
       $air = new healtboxCmd();
@@ -129,72 +81,33 @@ class healtbox extends eqLogic {
   }
 
   // Fonction exécutée automatiquement avant la suppression de l'équipement
-  public function preRemove() {
+  public function preRemove()
+  {
   }
 
   // Fonction exécutée automatiquement après la suppression de l'équipement
-  public function postRemove() {
+  public function postRemove()
+  {
   }
 
-  /*
-  * Permet de crypter/décrypter automatiquement des champs de configuration des équipements
-  * Exemple avec le champ "Mot de passe" (password)
-  public function decrypt() {
-    $this->setConfiguration('password', utils::decrypt($this->getConfiguration('password')));
-  }
-  public function encrypt() {
-    $this->setConfiguration('password', utils::encrypt($this->getConfiguration('password')));
-  }
-  */
-
-  /*
-  * Permet de modifier l'affichage du widget (également utilisable par les commandes)
-  public function toHtml($_version = 'dashboard') {}
-  */
-
-  /*
-  * Permet de déclencher une action avant modification d'une variable de configuration du plugin
-  * Exemple avec la variable "param3"
-  public static function preConfig_param3( $value ) {
-    // do some checks or modify on $value
-    return $value;
-  }
-  */
-
-  /*
-  * Permet de déclencher une action après modification d'une variable de configuration du plugin
-  * Exemple avec la variable "param3"
-  public static function postConfig_param3($value) {
-    // no return value
-  }
-  */
-
-  /*     * **********************Getteur Setteur*************************** */
 
 }
-class healtboxCmd extends cmd {
-	/*     * *************************Attributs****************************** */
-	
-	public static $_widgetPossibility = array('custom' => false);
-	
-	/*     * ***********************Methode static*************************** */
-	
-	/*     * *********************Methode d'instance************************* */
-	
-	public function execute($_options = array()) {
-		if ($this->getLogicalId() == 'refresh') {
-			$this->getEqLogic()->updateWeatherData();
-		}
-		return false;
-	}
-	
-	/*     * **********************Getteur Setteur*************************** */
+class healtboxCmd extends cmd
+{
+  /*     * *************************Attributs****************************** */
+
+  public static $_widgetPossibility = array('custom' => false);
+
+  /*     * ***********************Methode static*************************** */
+
+  /*     * *********************Methode d'instance************************* */
+
+  public function execute($_options = array())
+  {
+    if ($this->getLogicalId() == 'refresh') {
+      $this->getEqLogic()->updateWeatherData();
+    }
+    return false;
+  }
+
 }
-
-
-
- 
-
-  /*     * **********************Getteur Setteur*************************** */
-
-
