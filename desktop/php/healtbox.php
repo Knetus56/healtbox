47b7ae111a4361bf93e97ae69ac4799a78376728
Knetus56 <98704728+Knetus56@users.noreply.github.com>
2023-01-26 08:57:11 +0100

    Update healtbox.php

--- desktop/php/healtbox.php
+++ desktop/php/healtbox.php
@@ -204,6 +204,6 @@ $eqLogics = eqLogic::byType($plugin->getId());
 </div><!-- /.row row-overflow -->
 
 <!-- Inclusion du fichier javascript du plugin (dossier, nom_du_fichier, extension_du_fichier, id_du_plugin) -->
-<?php include_file('desktop', 'healtbox', 'js', 'healtbox');?>
+<?php include_file('desktop', 'healtbox', 'js', 'template');?>
 <!-- Inclusion du fichier javascript du core - NE PAS MODIFIER NI SUPPRIMER -->
-<?php include_file('core', 'plugin.healtbox', 'js');?>
+<?php include_file('core', 'plugin.template', 'js');?>
