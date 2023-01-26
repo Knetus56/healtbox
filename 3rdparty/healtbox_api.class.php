6c0d725f7d36be8c139ff71608d5d31885e8a115
Knetus56 <98704728+Knetus56@users.noreply.github.com>
2023-01-26 11:15:28 +0100

    Update healtbox_api.class.php

--- 3rdparty/healtbox_api.class.php
+++ 3rdparty/healtbox_api.class.php
@@ -1,15 +1,16 @@
 <?php
 class healtbox_api {
 
-	protected $_url_data = 'https://iam-api.dss.husqvarnagroup.net/api/v3/';
-	protected $_url_boost = 'https://amc-api.dss.husqvarnagroup.net/v1/';
 	protected $_ip;
+	protected $_url_data = '/v2/api/data/current';
+	protected $_url_boost = '/v2/api/boost/';
 	protected $_data;
 	protected $_dataBoost;
 
 	public function __construct($ip)
     {
         $this->setip($ip);
+        $this->getData();
     }
 
     public function setip($ip)
@@ -27,43 +28,56 @@ class healtbox_api {
         $session = curl_init();
 
         curl_setopt_array($session, [
-            CURLOPT_URL => "http://". $this->getip()."/v2/api/data/current",            
+            CURLOPT_URL => "http://". $this->getip().$this->_url_data,            
             CURLOPT_RETURNTRANSFER => true,   
             CURLOPT_HTTPHEADER => ["Content-Type: application/json"]
         ]);
        
 		$json = curl_exec($session);
 		curl_close($session);
-		return json_decode($json, true);
+		$this->_data = json_decode($json, true);
     }
  
 
+    public function getNbPiece(){
+       
+        return count($this->_data['room']);
+    } 
+   
+   
+   
+   
+   
+   
+   
+   
+   
     /*
-     * Méthode: PUT
-     * URL: https://api.healtbox.fr/public/v1/device/{deviceId}/pump
-     * {deviceId} est à remplacer par le numéro unique d’association ou par le mot clef my.
-     * pump peut prendre comme valeur « on », « off », et « auto »
-     */
-    public function putPompe(string $value){
-        $data = ["pump" => $value];
-        $curl = curl_init();
+    * Méthode: PUT
+    * URL: https://api.healtbox.fr/public/v1/device/{deviceId}/pump
+    * {deviceId} est à remplacer par le numéro unique d’association ou par le mot clef my.
+    * pump peut prendre comme valeur « on », « off », et « auto »
+    */
+   public function putPompe(string $value){
+       $data = ["pump" => $value];
+       $curl = curl_init();
 
-        curl_setopt_array($curl, [
-            CURLOPT_URL => "https://api.healtbox.fr/public/v1/device/my/pump",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_CUSTOMREQUEST => "PUT",
-            CURLOPT_POSTFIELDS => json_encode($data),
-            CURLOPT_HTTPHEADER => [
-                "X-API-TOKEN: ". $this->getApiToken(),
-                "Content-Type: application/json"
-            ]
-        ]);
+       curl_setopt_array($curl, [
+           CURLOPT_URL => "https://api.healtbox.fr/public/v1/device/my/pump",
+           CURLOPT_RETURNTRANSFER => true,
+           CURLOPT_CUSTOMREQUEST => "PUT",
+           CURLOPT_POSTFIELDS => json_encode($data),
+           CURLOPT_HTTPHEADER => [
+               "X-API-TOKEN: ". $this->getApiToken(),
+               "Content-Type: application/json"
+           ]
+       ]);
 
-        $response = curl_exec($curl);
+       $response = curl_exec($curl);
 
-        curl_close($curl);
-        return $response;
-    }
+       curl_close($curl);
+       return $response;
+   }
 
    
 }
