1ffc8143cf1d13a65f0bc397d3778a6d22bed457
Knetus56 <98704728+Knetus56@users.noreply.github.com>
2023-01-26 22:37:45 +0100

    Update healtbox.js

--- desktop/js/healtbox.js
+++ desktop/js/healtbox.js
@@ -73,7 +73,8 @@ function addCmdToTable(_cmd) {
   tr += '</td><td align="right">';
   if (is_numeric(_cmd.id)) {
     tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fas fa-cogs"></i></a> ';
-    tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> {{Tester}}</a>';
+  tr += '</td><td>';
+  tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> {{Tester}}</a>';
   }
   tr += '&nbsp; &nbsp; <i class="fas fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i>';
   tr += '</td></tr>';
