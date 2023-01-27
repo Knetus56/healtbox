299b8b667621c01b207fedc2de8511fe403e39d3
Knetus56 <knetus2@hotmail.fr>
2023-01-27 15:34:03 +0100

    Update healtbox.class.php

--- core/class/healtbox.class.php
+++ core/class/healtbox.class.php
@@ -21,160 +21,157 @@ require_once __DIR__ . '/../../3rdparty/healtbox_api.class.php';
 
 class healtbox extends eqLogic
 {
-  // ================================================================================
-  public static function cron()
-  {
+    // ================================================================================
+    public static function cron()
+    {
 
-    foreach (healtbox::byType('healtbox') as $eqLogic) {
-      try {
-        $eqLogic->updatehealtbox();
-      } catch (Exception $e) {
-        log::add('healtbox', 'info', $e->getMessage());
-      }
+        foreach (healtbox::byType('healtbox') as $eqLogic) {
+            try {
+                $eqLogic->updatehealtbox();
+            } catch (Exception $e) {
+                log::add('healtbox', 'info', $e->getMessage());
+            }
+        }
     }
-  }
-  // ================================================================================
-  public function updatehealtbox()
-  {
-    $api = new healtbox_api($this->getConfiguration('ip'));
-    $ap = $api->getNbPiece();
-    //   log::add('healtbox', 'info', $ap);
+    // ================================================================================
+    public function updatehealtbox()
+    {
+        $api = new healtbox_api($this->getConfiguration('ip'));
+        $ap = $api->getNbPiece();
+        //   log::add('healtbox', 'info', $ap);
 
+        $this->checkAndUpdateCmd('0:' . 'device_type', $api->getDevice());
 
-    $this->checkAndUpdateCmd('0:' . 'device_type', $api->getDevice());
+        for ($i = 1; $i <= $ap; $i++) {
 
-    for ($i = 1; $i <= $ap; $i++) {
+            $NamePiece = str_replace(" ", "_", $api->getNamePiece($i));
+            $this->checkAndUpdateCmd($i . ':' . $NamePiece . ':temperature', $api->getTemperature($i));
+            $this->checkAndUpdateCmd($i . ':' . $NamePiece . ':humidity', $api->getHumidity($i));
+            $this->checkAndUpdateCmd($i . ':' . $NamePiece . ':profil', $api->getProfil($i));
+            $this->checkAndUpdateCmd($i . ':' . $NamePiece . ':debit', $api->getDebit($i));
+            $CO2 = $api->isCO2($i);
+            if ($CO2) {
+                $this->checkAndUpdateCmd($i . ':' . $NamePiece . ':CO2', $api->getCO2($i));
+            }
+            $COV = $api->isCOV($i);
+            if ($COV) {
+                $this->checkAndUpdateCmd($i . ':' . $NamePiece . ':COV', $api->getCOV($i));
+            }
+        }
 
-      $NamePiece = str_replace(" ", "_", $api->getNamePiece($i));
-      $this->checkAndUpdateCmd($i . ':' . $NamePiece . ':temperature', $api->getTemperature($i));
-      $this->checkAndUpdateCmd($i . ':' . $NamePiece . ':humidity', $api->getHumidity($i));
-      $this->checkAndUpdateCmd($i . ':' . $NamePiece . ':profil', $api->getProfil($i));
-      $this->checkAndUpdateCmd($i . ':' . $NamePiece . ':debit', $api->getDebit($i));
-      $CO2 = $api->isCO2($i);
-      if ($CO2) {
-        $this->checkAndUpdateCmd($i . ':' . $NamePiece . ':CO2', $api->getCO2($i));
-      }
-      $COV = $api->isCOV($i);
-      if ($COV) {
-        $this->checkAndUpdateCmd($i . ':' . $NamePiece . ':COV', $api->getCOV($i));
-      }
+        $this->refreshWidget();
+    }
+    // ================================================================================
+    public function preInsert()
+    {
     }
 
-    $this->refreshWidget();
-  }
-  // ================================================================================
-  public function preInsert()
-  {
-  }
-
-  // ================================================================================
-  public function postInsert()
-  {
-  }
-
-  // ================================================================================
-  public function preUpdate()
-  {
-    if ($this->getConfiguration('ip') == '') {
-      throw new Exception(__('Veuillez entrer une IP', __FILE__));
+    // ================================================================================
+    public function postInsert()
+    {
     }
-  }
 
-  // ================================================================================
-  public function postUpdate()
-  {
-  }
-
-  // ================================================================================
-  public function preSave()
-  {
-  }
-
-  // ================================================================================
-  public function setLogical($i, $room, $Name, $Type, $Unit, $SubType)
-  {
-    $logic = $this->getCmd(null, $Name);
-    if (!is_object($logic)) {
-      $logic = new healtboxCmd();
+    // ================================================================================
+    public function preUpdate()
+    {
+        if ($this->getConfiguration('ip') == '') {
+            throw new Exception(__('Veuillez entrer une IP', __FILE__));
+        }
     }
-    $NamePiece = str_replace(" ", "_", $Name);
-    $logic->setName(__($room  .  ':'  .  $Name, __FILE__));
-    $logic->setLogicalId( $NamePiece .  '_'  . $i );
-    $logic->setEqLogic_id($this->getId());
-    $logic->setType($Type);
-    $logic->setUnite($Unit);
-    $logic->setSubType($SubType);
-    $logic->save();
-  }
 
-  // ================================================================================
-  public function postSave()
-  {
-    $api = new healtbox_api($this->getConfiguration('ip'));
-    $ap = $api->getNbPiece();
-    //   log::add('healtbox', 'info', $ap);
-
-    $this->setLogical(0, 'device_type','config', 'info', '', 'string');
-
-     for ($i = 1; $i <= $ap; $i++) {
-
-       $NamePiece = $api->getNamePiece($i);
-       $this->setLogical($i, $NamePiece , 'temperature', 'info', '°C', 'numeric');
-    //   $this->setLogical($i, $NamePiece . '_humidity', 'info', '%', 'numeric');
-    //   $this->setLogical($i, $NamePiece . '_debit', 'info', '%', 'numeric');
-    //   $this->setLogical($i, $NamePiece . '_profil', 'info', '', 'numeric');
-
-    //   $CO2 = $api->isCO2($i);
-    //   if ($CO2) {
-    //     $this->setLogical($i, $NamePiece . ':CO2', 'info', 'ppm', 'numeric');
-    //   }
-
-    //   $COV = $api->isCOV($i);
-    //   if ($COV) {
-    //     $this->setLogical($i, $NamePiece . ':COV', 'info', 'ppm', 'numeric');
-    //   }
-
-
-    //   $this->setLogical($i, $NamePiece . ':boostON', 'action', '', 'other');
-    //   $this->setLogical($i, $NamePiece . ':boostOFF', 'action', '', 'other');
-    //   $this->setLogical($i, $NamePiece . ':changeProfil', 'action', '', 'other');
-
-     }
-
-    if ($this->getIsEnable() == 1) {
-      $this->updatehealtbox();
+    // ================================================================================
+    public function postUpdate()
+    {
     }
-  }
 
+    // ================================================================================
+    public function preSave()
+    {
+    }
 
+    // ================================================================================
+    public function setLogical($i, $room, $Name, $Type, $Unit, $SubType)
+    {
+
+        $NamePiece = str_replace(" ", "_", $room);
+        $NamePiece = $i . ':' . $room;
+
+        $logic = $this->getCmd(null, $NamePiece);
+        if (!is_object($logic)) {
+            $logic = new healtboxCmd();
+        }
+        $logic->setName(__($room . ':' . $Name, __FILE__));
+        $logic->setLogicalId($NamePiece);
+        $logic->setEqLogic_id($this->getId());
+        $logic->setType($Type);
+        $logic->setUnite($Unit);
+        $logic->setSubType($SubType);
+        $logic->save();
+    }
+
+    // ================================================================================
+    public function postSave()
+    {
+        $api = new healtbox_api($this->getConfiguration('ip'));
+        $ap = $api->getNbPiece();
+        //   log::add('healtbox', 'info', $ap);
+
+        $this->setLogical(0, 'device_type', 'config', 'info', '', 'string');
+
+        for ($i = 1; $i <= $ap; $i++) {
+
+            $NamePiece = $api->getNamePiece($i);
+            $this->setLogical($i, $NamePiece, 'temperature', 'info', '°C', 'numeric');
+            //   $this->setLogical($i, $NamePiece . '_humidity', 'info', '%', 'numeric');
+            //   $this->setLogical($i, $NamePiece . '_debit', 'info', '%', 'numeric');
+            //   $this->setLogical($i, $NamePiece . '_profil', 'info', '', 'numeric');
+
+            //   $CO2 = $api->isCO2($i);
+            //   if ($CO2) {
+            //     $this->setLogical($i, $NamePiece . ':CO2', 'info', 'ppm', 'numeric');
+            //   }
+
+            //   $COV = $api->isCOV($i);
+            //   if ($COV) {
+            //     $this->setLogical($i, $NamePiece . ':COV', 'info', 'ppm', 'numeric');
+            //   }
+
+            //   $this->setLogical($i, $NamePiece . ':boostON', 'action', '', 'other');
+            //   $this->setLogical($i, $NamePiece . ':boostOFF', 'action', '', 'other');
+            //   $this->setLogical($i, $NamePiece . ':changeProfil', 'action', '', 'other');
+
+        }
+
+        if ($this->getIsEnable() == 1) {
+            $this->updatehealtbox();
+        }
+    }
 
 }
 // ================================================================================
 class healtboxCmd extends cmd
 {
-  public static $_widgetPossibility = ['custom' => false];
+    public static $_widgetPossibility = ['custom' => false];
 
+    public function execute($_options = array())
+    {
 
-  public function execute($_options = array())
-  {
+        if ($this->getType() == 'info') {
+            return;
+        }
 
-    if ($this->getType() == 'info') {
-      return;
+        $api = new healtbox_api($this->getConfiguration('ip'));
+        $request = $this->getConfiguration("request", "");
+        $r = $this->getLogicalId();
+
+        $p = explode(" ", $r);
+        $id = $p[0];
+        $req = $p[2];
+        if ($req == 'changeProfil') {
+            $api->changeProfil($id, $request);
+
+        }
+
+        return false;
     }
-
-    $api = new healtbox_api($this->getConfiguration('ip'));
-    $request = $this->getConfiguration("request", "");
-    $r = $this->getLogicalId();
-
-    $p = explode(" ", $r);
-    $id = $p[0];
-    $req = $p[2];
-    if ($req == 'changeProfil') {
-      $api->changeProfil($id, $request);
-
-
-    }
-
-    return false;
-  }
 }
