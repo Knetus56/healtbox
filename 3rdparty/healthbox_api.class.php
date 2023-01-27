889220bc0edf177cfbd6a9e6169106967b5c6b54
Knetus56 <knetus2@hotmail.fr>
2023-01-27 17:52:29 +0100

    Update healthbox_api.class.php

--- 3rdparty/healthbox_api.class.php
+++ 3rdparty/healthbox_api.class.php
@@ -46,7 +46,7 @@ class healthbox_api
     public function put($url, $data)
     {
         log::add('healthbox', 'info', '_ip  :  ' . $this->_ip);
-        log::add('healthbox', 'info', 'data  :  ' . $data);
+ 
 
         $curl = curl_init();
         curl_setopt_array($curl, [
