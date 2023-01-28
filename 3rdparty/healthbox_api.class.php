e63f8c3db5063ac63b8ee197e272feefa93200fc
Knetus56 <98704728+Knetus56@users.noreply.github.com>
2023-01-28 13:41:35 +0100

    Update healthbox_api.class.php

--- 3rdparty/healthbox_api.class.php
+++ 3rdparty/healthbox_api.class.php
@@ -96,8 +96,7 @@ class healthbox_api
     // ================================================================================
     public function getProfil($json)
     {
-        log::add('healthbox', 'info', print_r($json['parameter']['profile_name']));
-        return array_search($json['parameter']['profile_name'], self::PROFIL);
+        return $json['parameter']['profile_name'];
     }
     // ================================================================================
     public function getSensor($json, $param)
